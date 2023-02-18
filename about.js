dd03563b9546cd0c2150fd39b537f382d44f9435
Keldon Lopez <keldon.lopz@gmail.com>
2023-02-18 15:26:56 -0700

    finished part 1 and part 2

--- about.js
+++ about.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('form submit');
 }
 
+function compliment(evt) {
+	evt.preventDefault();
+	
+	alert('The cat thinks you are pretty and or handsome');
+}
 
 let form = document.querySelector('#contact');
+let catPic = document.querySelector('#catPic');
 
 form.addEventListener('submit', handleSubmit);
+
+catPic.addEventListener('mouseover', compliment);
