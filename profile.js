c3501aa7dcce4d829dfca1cbf87b68ad4133864b
Keldon Lopez <keldon.lopz@gmail.com>
2023-02-19 14:54:05 -0700

    Added neighborhood

--- profile.js
+++ profile.js
@@ -1,9 +1,11 @@
-const favColor = document.querySelectorAll('#color');
-const favPlace = document.querySelectorAll('#place');
-const favRitual = document.querySelectorAll('#ritual');
+console.log("profile.js")
+const favColor = document.querySelector('#color');
+const favPlace = document.querySelector('#place');
+const favRitual = document.querySelector('#ritual');
 const crimson = () => alert("Keldon's favorite color is Crimson");
 const hawaii = () => alert("Keldon's favorite place is Hawaii");
 const spaghetti = () => alert("Keldon's favorite ritual is mixing spaghetti before eating it");
+
 favColor.addEventListener('click', crimson );
 favPlace.addEventListener('click', hawaii );
 favRitual.addEventListener('click', spaghetti );
